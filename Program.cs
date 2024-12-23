310a28877592bab06e7573f749dca6c3ae181033
SIMONIN Mathieu <simonin.math@gmail.com>
2024-12-23 21:57:00 +0100

    #BM [Back] - Project setting up

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+public class RaylibGame
+{
+    public static int Main()
+    {
+        InitWindow(1280, 1024, "build_master");
+        SetTargetFPS(60);
+
+        InitAudioDevice();
+
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(Color.RayWhite);
+            DrawText("build_master", 190, 200, 20, Color.Magenta);
+            EndDrawing();
+        }
+
+        CloseAudioDevice();
+        CloseWindow();
+        return 0;
+    }
+}
